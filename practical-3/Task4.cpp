0ee938d9c195e51611e5a515a60e48bce89c8cb0
Sachinaswal29 <91174800+Sachinaswal29@users.noreply.github.com>
2021-10-03 10:22:39 +0530

    Add files via upload

---
+++ practical-3/Task4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    string a;
+    cout<<"Enter\n";
+    getline(cin,a);
+    cout<<a;
+    return 0;
+    }
+    
+//cin
+//It is used to read a word or character.
+//It terminates on encountering whitespace or endline character('\n')
+
+//The C++ getline() is a standard library function that is used to read a string or a line from an input stream.
+//It is a part of the <string> header.The getline()function extracts characters from the input stream and appends it to the string object until the delimiting character is encounterd
